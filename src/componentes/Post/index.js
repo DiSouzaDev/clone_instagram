6d980dbf05a98f93f4dede3a47dfafc3f0846981
Diogo de Souza Barbosa <88636833+Diogo-Javax888@users.noreply.github.com>
2022-10-19 16:15:57 -0300

    API implementada
    Implementação feita com a implementação da API do github com novos conteúdos

--- src/componentes/Post/index.js
+++ src/componentes/Post/index.js
@@ -9,56 +9,149 @@ import { IconContext } from 'react-icons'
 export function Post() {
     return (
         <>
-            <header className='header-posts'>
-                <div className="infos-post">
-                    <img className='img-user' src={user} />
-                    <p>diogosouzabarbosa</p>
-                </div>
-                <FiMoreHorizontal />
-            </header>
-
-            <div className='img-post'>
-                <img className='img-user-post' src={user} />
-            </div>
-
-            <div className='conteudo-post'>
-                <IconContext.Provider value={{ size: '30px' }}>
-                    <section className='icons-post'>
-                        <div className='icons-1'>
-                            <div className='icons'><IoMdHeartEmpty /></div>
-                            <div className='icons'><BsChat /></div>
-                            <div className='icons'><FiSend /></div>
-                        </div>
-                        <div className='icons'><BsBookmark /></div>
-                    </section>
-                </IconContext.Provider>
-
-                <section className='like'>
-                    <span>251 curtidas</span>
-                </section>
-
-                <div className='legend'>
-                    <p>
-                        <span>dipziDanger</span> Foto massa parabéns lindo gostoso maravilhoso mais texto para testes 60% de desconto e mais textoooos
-                    </p>
-                </div>
-
-                <div className='horas'>
-                    <time>Há 1 hora</time>
-                </div>
-
-                <div className='coment'>
-                    <div className='fake-coment'>
-
-                        <IconContext.Provider value={{ size: '25px' }}>
-                            <div className='icons'><BsEmojiSmile /></div>
-                        </IconContext.Provider>
-
-                        <input placeholder='Adicione um comentário...'/>
+            <div className='tudo-post' >
+                <header className='header-posts'>
+                    <div className="infos-post">
+                        <img className='img-user' src={user} />
+                        <p>diogoSouzaBarbosa</p>
                     </div>
-                    <button>Publicar</button>
+                    <FiMoreHorizontal />
+                </header>
+
+                <div className='img-post'>
+                    <img className='img-user-post' src={user} />
+                </div>
+                <div className='conteudo-post'>
+                    <IconContext.Provider value={{ size: '30px' }}>
+                        <section className='icons-post'>
+                            <div className='icons-1'>
+                                <div className='icons'><IoMdHeartEmpty /></div>
+                                <div className='icons'><BsChat /></div>
+                                <div className='icons'><FiSend /></div>
+                            </div>
+                            <div className='icons'><BsBookmark /></div>
+                        </section>
+                    </IconContext.Provider>                            
+
+                    <section className='like'>
+                        <span>251 curtidas</span>
+                    </section>
+
+                    <div className='legend'>
+                        <p>
+                            <span>diogoSouzaBarbosa</span> Moletom novo :)
+                        </p>
+                    </div>
+
+                    <div className='horas'>
+                        <time>Há 1 hora</time>
+                    </div>
+
+                    <div className='coment'>
+                        <div className='fake-coment'>
+
+                            <IconContext.Provider value={{ size: '25px' }}>
+                                <div className='icons'><BsEmojiSmile /></div>
+                            </IconContext.Provider>
+
+                            <input placeholder='Adicione um comentário...' />
+                        </div>
+                        <button className='publicar'>Publicar</button>
+                    </div>
                 </div>
             </div>
         </>
     )
 }
+
+// import user from './img/user.png'
+// import './style.css'
+// import { FiMoreHorizontal } from 'react-icons/fi'
+// import { IoMdHeartEmpty } from 'react-icons/io'
+// import { BsChat, BsBookmark, BsEmojiSmile } from 'react-icons/bs'
+// import { FiSend } from 'react-icons/fi'
+// import { IconContext } from 'react-icons'
+// import { useEffect, useState } from 'react'
+// import { Story } from '../Story'
+
+// export function Post() {
+
+//     const [post, setPost] = useState([])
+
+//     const [limite, setLimit] = useState(20)
+
+//     const slice = post.slice(0, limite)
+
+//     useEffect(() => {
+//         fetch(`https://api.github.com/users/Diogo-Javax888/followers`)
+//             .then((response) => {
+//                 return response.json()
+//             })
+//             .then((result) => {
+//                 setPost(result)
+//             })
+
+//             .catch((err) => {
+//                 throw new Error(err)
+//             })
+
+//     }, [])
+
+//     return (
+//         <>
+//             {slice.map((post, key) => (
+//                 <div className='tudo-post' key={key}>
+//                     <header className='header-posts'>
+//                         <div className="infos-post">
+//                             <img className='img-user' src={`https://github.com/${post.login}.png`} />
+//                             <p>{post.login}</p>
+//                         </div>
+//                         <FiMoreHorizontal />
+//                     </header>
+
+//                     <div className='img-post'>
+//                         <img className='img-user-post' src={`https://github.com/${post.login}.png`} />
+//                     </div>
+//                     <div className='conteudo-post'>
+//                         <IconContext.Provider value={{ size: '30px' }}>
+//                             <section className='icons-post'>
+//                                 <div className='icons-1'>
+//                                     <div className='icons'><IoMdHeartEmpty /></div>
+//                                     <div className='icons'><BsChat /></div>
+//                                     <div className='icons'><FiSend /></div>
+//                                 </div>
+//                                 <div className='icons'><BsBookmark /></div>
+//                             </section>
+//                         </IconContext.Provider>
+
+//                         <section className='like'>
+//                             <span>251 curtidas</span>
+//                         </section>
+
+//                         <div className='legend'>
+//                             <p>
+//                                 <span>{post.login}</span> Esse é meu github, me segue lá: {post.html_url}
+//                             </p>
+//                         </div>
+
+//                         <div className='horas'>
+//                             <time>Há 1 hora</time>
+//                         </div>
+
+//                         <div className='coment'>
+//                             <div className='fake-coment'>
+
+//                                 <IconContext.Provider value={{ size: '25px' }}>
+//                                     <div className='icons'><BsEmojiSmile /></div>
+//                                 </IconContext.Provider>
+
+//                                 <input placeholder='Adicione um comentário...' />
+//                             </div>
+//                             <button className='publicar'>Publicar</button>
+//                         </div>
+//                     </div>
+//                 </div>
+//             ))}
+//         </>
+//     )
+// }
--- src/componentes/Story/index.js
+++ src/componentes/Story/index.js
@@ -1,41 +1,45 @@
 import './style.css'
 import user from './img/user.png'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export function Story() {
 
-    // const [story, setStory] = useState([])
+    const [story, setStory] = useState([])
+
+    const [limite, setLimite] = useState(5)
+
+    const slice = story.slice(0, limite)
+
+
+    useEffect(() => {
+        fetch(`https://api.github.com/users/Diogo-Javax888/followers`)
+            .then((response) => {
+                return response.json()
+            })
+            .then((result) => {
+                setStory(result)
+            })
+
+            .catch((err) => {
+                throw new Error(err)
+            })
+
+    }, [])
 
     return (
-        <div className='container'>
+        <div className='every'>
+            {slice.map((story, key) => (
+                <div className='container' key={key}>
 
-            <div className='user-element'>
-                <div>
-                    <img className='image-user-story' src={user} />
+                    <div className='user-element'>
+                        <div>
+                            <img className='image-user-story' src={`https://github.com/${story.login}.png`} />
+                        </div>
+                        <span>{story.login}</span>
+                    </div>
+
                 </div>
-                <span>
-                    DiogoSo
-                </span>
-            </div>
-
-            <div className='user-element'>
-                <div>
-                    <img className='image-user-story' src={user} />
-                </div>
-                <span>
-                    DiogoSo
-                </span>
-            </div>
-
-            <div className='user-element'>
-                <div>
-                    <img className='image-user-story' src={user} />
-                </div>
-                <span>
-                    DiogoSo
-                </span>
-            </div>
-
+            ))}
         </div>
     )
 }
